512c1c729c9a23b55be245d16b376927a60aa017
hsa0604 <110574731+hsa0604@users.noreply.github.com>
2024-07-05 19:47:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,12 +21,15 @@ contadores[1].textContent = calculaTempo(tempoObjetivo2);
 contadores[2].textContent = calculaTempo(tempoObjetivo3);
 contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
-function calculaTempo(tempoObjetivo1) {
-
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     
     segundos %= 60;
     minutos %= 60;

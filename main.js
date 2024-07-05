19d7827f4a09f89f6360e9f6a44c2df8da6ea068
hsa0604 <110574731+hsa0604@users.noreply.github.com>
2024-07-05 19:38:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll.("contador);
+const tempoObjetivo1 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
